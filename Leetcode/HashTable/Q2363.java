84062093c136ded926971e4801dc906bf2cb68dd
Aniket Sharma <aniketsharma77722@gmail.com>
2023-06-16 12:48:54 +0530

    Merge Similar Items

--- Leetcode/HashTable/Q2363.java
+++ Leetcode/HashTable/Q2363.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Q2363<E> {
+public class Q2363 {
     public static void main(String[] args) {
         int[][] items1 = {
                 { 1, 1 },
