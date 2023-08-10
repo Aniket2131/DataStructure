4350136bda5862e2d776f07c624a5b49182f02ae
Aniket2131 <aniketsharma77722@gmail.com>
2023-08-10 22:50:12 +0530

    Search in Rotated Sorted Array II

---
+++ Leetcode/Arrays/Q81.java
@@ -0,0 +1,33 @@
+public class Q81 {
+    public static void main(String[] args) {
+        int nums[] = {2,5,6,0,0,1,2}, target = 0;
+        System.out.println(search(nums, target));
+    }
+
+    public static boolean search(int[] nums, int target) {
+        int start = 0, end = nums.length - 1, mid = -1;
+        while(start <= end) {
+            mid = (start + end) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[mid] < nums[end] || nums[mid] < nums[start]) {
+                if (target > nums[mid] && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            } else if (nums[mid] > nums[start] || nums[mid] > nums[end]) {
+                if (target < nums[mid] && target >= nums[start]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                end--;
+            }
+        }
+        
+        return false;
+    }
+}
