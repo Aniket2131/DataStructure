0af5adaa307c7353400a03c78e02a732845e12bb
Aniket2131 <aniketsharma77722@gmail.com>
2023-08-14 23:23:31 +0530

    2 Different elements in the array

---
+++ GFG/Bit/NonRepeatingNumbers.java
@@ -0,0 +1,37 @@
+public class NonRepeatingNumbers{
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 2, 1, 4};
+        System.out.println(singleNumber(nums));
+    }
+
+    public static int[] singleNumber(int[] nums)
+    {
+        // Code here
+        int num = 0;
+        for(int el: nums){
+            num = num ^ el; 
+        }
+        
+        int ans1 = 0;
+        int ans2 = 0;
+        int rsb = (num & -num);
+        for(int el : nums){
+            if((el & rsb) == 0){
+                ans1 = ans1 ^ el;
+            }
+            else{
+                ans2 = ans2 ^ el;
+            }
+        }
+        int[] ans = new int[2];
+        if(ans1 > ans2){
+            ans[0] = ans2;
+            ans[1] = ans1;
+        }
+        else{
+            ans[0] = ans1;
+            ans[1] = ans2;
+        }
+        return ans;
+    }
+}
