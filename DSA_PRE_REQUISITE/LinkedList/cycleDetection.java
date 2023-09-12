a5f4bd09b19c4bc24293063ea9c492c47044091c
Aniket2131 <aniketsharma77722@gmail.com>
2023-09-12 22:55:33 +0530

    Floyd's Algo

---
+++ DSA_PRE_REQUISITE/LinkedList/cycleDetection.java
@@ -0,0 +1,26 @@
+public class cycleDetection {
+    public static void main(String[] args) {
+
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode Head){
+        ListNode slow = Head;
+        ListNode fast = Head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(fast == slow) return true;
+        }
+        return false;
+    }
+}
